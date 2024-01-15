31884d1f6c7f44902d6a2383439df811244f605c
LeeDaeGyeong <dleorud1027@gmail.com>
2024-01-15 21:24:47 +0900

    feat : router 세팅

---
+++ calendar/src/pages/home-page.tsx
@@ -0,0 +1,5 @@
+const HomePage = () => {
+  return <div>zzzzz</div>;
+};
+
+export default HomePage;
---
+++ calendar/src/pages/side-page.tsx
@@ -0,0 +1,5 @@
+const SidePage = () => {
+  return <div>side</div>;
+};
+
+export default SidePage;
